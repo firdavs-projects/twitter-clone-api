e6968974f690645f125335faf1924688ccf4a90f
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2022-08-25 15:00:06 +0500

    feat: upload images route

---
+++ config/connectStorage.js
@@ -0,0 +1,18 @@
+const config = require("config");
+const cloudinary = require("cloudinary");
+
+const connectStorage = async () => {
+  try {
+    await cloudinary.config({
+      cloud_name: config.get('cloud_name'),
+      api_key: config.get('api_key'),
+      api_secret: config.get('api_secret'),
+    });
+    console.log('Connected to the Cloudinary')
+  } catch (e) {
+    console.error('Server Error', e.message)
+    process.exit(1)
+  }
+}
+
+module.exports = connectStorage
