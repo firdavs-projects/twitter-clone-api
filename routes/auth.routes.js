d363c9412bcf10c588e850aa3e85fad83b7e0b3d
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2022-09-03 19:13:39 +0500

    fix: logout route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -127,10 +127,13 @@ router.post(
 
 router.get(
     '/logout',
-    authMiddleware,
     async (req, res) => {
       try {
         const token = req.headers.authorization.split(' ')[1]
+        if (!token) {
+          return res.status(401).json({message: 'Пользователь не авторизован'})
+        }
+
         const expired = new Expired({
           token
         })
